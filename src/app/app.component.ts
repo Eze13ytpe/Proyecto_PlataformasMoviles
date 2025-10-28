b3843e5c68270eebaa931fd92322a02661e4b3e3
Eze13ytpe <ezequielcipo2007@gmail.com>
2025-10-28 18:20:31 -0300

    creacion de componentes (no  anda el navbar)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { NavbarComponent } from "./componentes/navbar/navbar.component";
+import { FooterComponent } from "./componentes/footer/footer.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  imports: [RouterLink, IonApp, IonRouterOutlet, NavbarComponent, FooterComponent],
 })
 export class AppComponent {
   constructor() {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,29 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    path: 'inicio',
+    loadComponent: () => import('./componentes/inicio/inicio.component').then(m => m.InicioComponent),
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'inicio',
     pathMatch: 'full',
   },
+{
+    path: 'productos',
+    loadComponent: () => import('./componentes/productos/productos.component').then(m => m.ProductosComponent),
+  },
+  {
+    path: 'favoritos',
+    loadComponent: () => import('./componentes/favoritos/favoritos.component').then(m => m.FavoritosComponent),
+  },
+  {
+    path: 'carrito',
+    loadComponent: () => import('./componentes/carrito/carrito.component').then(m => m.CarritoComponent),
+  },
+   {
+    path: 'contacto',
+    loadComponent: () => import('./componentes/contactos/contactos.component').then(m => m.ContactosComponent),
+  },
+
 ];
